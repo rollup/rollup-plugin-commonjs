9687b2db3d64e88ae629017d4d44846bb59a3991
Rich-Harris <richard.a.harris@gmail.com>
2015-12-05 13:04:32 -0500

    reinstate named exports from object literals test

---
+++ test/samples/named-exports-from-object-literal/main.js
@@ -0,0 +1,5 @@
+import { a, b, c } from './other.js';
+
+assert.equal( a, 1 );
+assert.equal( b, 2 );
+assert.equal( c, 3 );
---
+++ test/samples/named-exports-from-object-literal/other.js
@@ -0,0 +1,9 @@
+var a = require( './a.js' );
+var b = 2;
+
+module.exports = {
+  a: a,
+  b: b,
+  c: a + b,
+  2: 1 + 1
+};
