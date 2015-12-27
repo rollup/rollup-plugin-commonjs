46154e00569ab216ecf756108801106b4c4ac40f
Rich-Harris <richard.a.harris@gmail.com>
2015-12-27 16:08:10 -0500

    update deps, es6ify tests

--- test/test.js
+++ test/test.js
@@ -1,66 +1,66 @@
-var path = require( 'path' );
-var assert = require( 'assert' );
-var SourceMapConsumer = require( 'source-map' ).SourceMapConsumer;
-var rollup = require( 'rollup' );
-var commonjs = require( '..' );
+import * as path from 'path';
+import * as assert from 'assert';
+import { SourceMapConsumer } from 'source-map';
+import { rollup } from 'rollup';
+import commonjs from '..';
 
 process.chdir( __dirname );
 
 function executeBundle ( bundle ) {
-	var generated = bundle.generate({
+	const generated = bundle.generate({
 		format: 'cjs'
 	});
 
-	var fn = new Function ( 'module', 'assert', generated.code );
-	var module = {};
+	const fn = new Function ( 'module', 'assert', generated.code );
+	let module = {};
 
 	fn( module, assert );
 
 	return module;
 }
 
-describe( 'rollup-plugin-commonjs', function () {
-	it( 'converts a basic CommonJS module', function () {
-		return rollup.rollup({
+describe( 'rollup-plugin-commonjs', () => {
+	it( 'converts a basic CommonJS module', () => {
+		return rollup({
 			entry: 'samples/basic/main.js',
 			plugins: [ commonjs() ]
-		}).then( function ( bundle ) {
+		}).then( bundle => {
 			assert.equal( executeBundle( bundle ).exports, 42 );
 		});
 	});
 
-	it( 'converts a CommonJS module that mutates exports instead of replacing', function () {
-		return rollup.rollup({
+	it( 'converts a CommonJS module that mutates exports instead of replacing', () => {
+		return rollup({
 			entry: 'samples/exports/main.js',
 			plugins: [ commonjs() ]
-		}).then( function ( bundle ) {
+		}).then( bundle => {
 			assert.equal( executeBundle( bundle ).exports, 'BARBAZ' );
 		});
 	});
 
-	it( 'converts inline require calls', function () {
-		return rollup.rollup({
+	it( 'converts inline require calls', () => {
+		return rollup({
 			entry: 'samples/inline/main.js',
 			plugins: [ commonjs() ]
-		}).then( function ( bundle ) {
+		}).then( bundle => {
 			assert.equal( executeBundle( bundle ).exports(), 2 );
 		});
 	});
 
-	it( 'generates a sourcemap', function () {
-		return rollup.rollup({
+	it( 'generates a sourcemap', () => {
+		return rollup({
 			entry: 'samples/sourcemap/main.js',
 			plugins: [ commonjs({ sourceMap: true }) ]
-		}).then( function ( bundle ) {
-			var generated = bundle.generate({
+		}).then( bundle => {
+			const generated = bundle.generate({
 				format: 'cjs',
 				sourceMap: true,
 				sourceMapFile: path.resolve( 'bundle.js' )
 			});
 
-			var smc = new SourceMapConsumer( generated.map );
+			const smc = new SourceMapConsumer( generated.map );
 
-			var loc = smc.originalPositionFor({ line: 5, column: 17 }); // 42
+			let loc = smc.originalPositionFor({ line: 5, column: 17 }); // 42
 			assert.equal( loc.source, 'samples/sourcemap/foo.js' );
 			assert.equal( loc.line, 1 );
 			assert.equal( loc.column, 15 );
@@ -72,97 +72,97 @@ describe( 'rollup-plugin-commonjs', function () {
 		});
 	});
 
-	it( 'finds index.js files', function () {
-		return rollup.rollup({
+	it( 'finds index.js files', () => {
+		return rollup({
 			entry: 'samples/index/main.js',
 			plugins: [ commonjs() ]
 		}).then( executeBundle );
 	});
 
-	it( 'handles reassignments to imports', function () {
-		return rollup.rollup({
+	it( 'handles reassignments to imports', () => {
+		return rollup({
 			entry: 'samples/reassignment/main.js',
 			plugins: [ commonjs() ]
 		}).then( executeBundle );
 	});
 
-	it( 'handles imports with a trailing slash', function () {
+	it( 'handles imports with a trailing slash', () => {
 		// yes this actually happens. Not sure why someone would do this
 		// https://github.com/nodejs/readable-stream/blob/077681f08e04094f087f11431dc64ca147dda20f/lib/_stream_readable.js#L125
-		return rollup.rollup({
+		return rollup({
 			entry: 'samples/trailing-slash/main.js',
 			plugins: [ commonjs() ]
 		}).then( executeBundle );
 	});
 
-	it( 'handles imports with a non-extension dot', function () {
-		return rollup.rollup({
+	it( 'handles imports with a non-extension dot', () => {
+		return rollup({
 			entry: 'samples/dot/main.js',
 			plugins: [ commonjs() ]
 		}).then( executeBundle );
 	});
 
-	it( 'handles shadowed require', function () {
-		return rollup.rollup({
+	it( 'handles shadowed require', () => {
+		return rollup({
 			entry: 'samples/shadowing/main.js',
 			plugins: [ commonjs() ]
 		}).then( executeBundle );
 	});
 
-	it( 'identifies named exports', function () {
-		return rollup.rollup({
+	it( 'identifies named exports', () => {
+		return rollup({
 			entry: 'samples/named-exports/main.js',
 			plugins: [ commonjs() ]
 		}).then( executeBundle );
 	});
 
-	it( 'handles references to `global`', function () {
-		return rollup.rollup({
+	it( 'handles references to `global`', () => {
+		return rollup({
 			entry: 'samples/global/main.js',
 			plugins: [ commonjs() ]
-		}).then( function ( bundle ) {
-			var generated = bundle.generate({
+		}).then( bundle => {
+			const generated = bundle.generate({
 				format: 'cjs'
 			});
 
-			var window = {};
+			let window = {};
 
-			var fn = new Function ( 'window', 'module', generated.code );
+			const fn = new Function ( 'window', 'module', generated.code );
 			fn( window, {} );
 
 			assert.equal( window.foo, 'bar', generated.code );
 		});
 	});
 
-	it( 'handles transpiled CommonJS modules', function () {
-		return rollup.rollup({
+	it( 'handles transpiled CommonJS modules', () => {
+		return rollup({
 			entry: 'samples/corejs/literal-with-default.js',
 			plugins: [ commonjs() ]
-		}).then( function ( bundle ) {
-			var generated = bundle.generate({
+		}).then( bundle => {
+			const generated = bundle.generate({
 				format: 'cjs'
 			});
 
-			var module = { exports: {} };
+			let module = { exports: {} };
 
-			var fn = new Function ( 'module', 'exports', generated.code );
+			const fn = new Function ( 'module', 'exports', generated.code );
 			fn( module, module.exports );
 
 			assert.equal( module.exports, 'foobar', generated.code );
 		});
 	});
 
-	it( 'does not export __esModule', function () {
-		return rollup.rollup({
+	it( 'does not export __esModule', () => {
+		return rollup({
 			entry: 'samples/__esModule/main.js',
 			plugins: [ commonjs() ]
-		}).then( function ( bundle ) {
-			var generated = bundle.generate({
+		}).then( bundle => {
+			const generated = bundle.generate({
 				format: 'cjs'
 			});
 
-			var fn = new Function ( 'module', 'exports', generated.code );
-			var module = { exports: {} };
+			const fn = new Function ( 'module', 'exports', generated.code );
+			let module = { exports: {} };
 
 			fn( module, module.exports );
 
