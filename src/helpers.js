a5caa8144af5a78cd69efc5757790ddc1743a062
Rich Harris <richard.a.harris@gmail.com>
2016-08-30 14:34:49 -0400

    remove unused helper

--- src/helpers.js
+++ src/helpers.js
@@ -1,12 +1,7 @@
 export const HELPERS_ID = '\0commonjsHelpers';
 
 export const HELPERS = `
-export var commonjsGlobal = typeof window !== 'undefined' ? window : typeof global !== 'undefined' ? global : typeof self !== 'undefined' ? self : {}
-
-export function interopDefault(x) {
-	if ( !x || typeof x !== 'object' || !x.default ) return x;
-	return x['default'];
-}
+export var commonjsGlobal = typeof window !== 'undefined' ? window : typeof global !== 'undefined' ? global : typeof self !== 'undefined' ? self : {};
 
 export function unwrapExports (x) {
 	return x && x.__esModule ? x['default'] : x;
