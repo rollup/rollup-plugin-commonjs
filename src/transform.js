ad8776488e39959c8bc93f45f2eeee36c3a86a75
Lukas Taegert <lukas.taegert@tngtech.com>
2018-10-05 10:09:36 +0200

    Resolve #348

--- src/transform.js
+++ src/transform.js
@@ -46,11 +46,18 @@ export function hasCjsKeywords(code, ignoreGlobal) {
 export function checkEsModule(parse, code, id) {
 	const ast = tryParse(parse, code, id);
 
-	// if there are top-level import/export declarations, this is ES not CommonJS
 	let isEsModule = false;
 	for (const node of ast.body) {
-		if (node.type === 'ExportDefaultDeclaration') return {isEsModule: true, hasDefaultExport: true, ast};
-		if (importExportDeclaration.test(node.type)) isEsModule = true;
+		if (node.type === 'ExportDefaultDeclaration')
+			return { isEsModule: true, hasDefaultExport: true, ast };
+		if (node.type === 'ExportNamedDeclaration') {
+			isEsModule = true;
+			for (const specifier of node.specifiers) {
+				if (specifier.exported.name === 'default') {
+					return { isEsModule: true, hasDefaultExport: true, ast };
+				}
+			}
+		} else if (importExportDeclaration.test(node.type)) isEsModule = true;
 	}
 
 	return { isEsModule, hasDefaultExport: false, ast };
