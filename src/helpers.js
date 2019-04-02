d93d838e04f932bae0dc99cf94ae30aabe47d88d
Tim Caswell <tim@creationix.com>
2019-04-02 01:58:27 -0500

    Make generated helper file have .js extension in preserveModules mode (#373)
    * Make generated helper file have .js extension in preserveModules mode
    
    * Support for new globalThis global

--- src/helpers.js
+++ src/helpers.js
@@ -1,11 +1,11 @@
 export const PROXY_PREFIX = '\0commonjs-proxy:';
 export const EXTERNAL_PREFIX = '\0commonjs-external:';
-export const HELPERS_ID = '\0commonjsHelpers';
+export const HELPERS_ID = '\0commonjsHelpers.js';
 
 // `x['default']` is used instead of `x.default` for backward compatibility with ES3 browsers.
 // Minifiers like uglify will usually transpile it back if compatibility with ES3 is not enabled.
 export const HELPERS = `
-export var commonjsGlobal = typeof window !== 'undefined' ? window : typeof global !== 'undefined' ? global : typeof self !== 'undefined' ? self : {};
+export var commonjsGlobal = typeof globalThis !== 'undefined' ? globalThis : typeof window !== 'undefined' ? window : typeof global !== 'undefined' ? global : typeof self !== 'undefined' ? self : {};
 
 export function commonjsRequire () {
 	throw new Error('Dynamic requires are not currently supported by rollup-plugin-commonjs');
