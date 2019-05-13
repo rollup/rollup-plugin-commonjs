f0e51abc67ed9fc9f2dea4af28e66d131c816f24
Lukas Taegert-Atkinson <lukas.taegert-atkinson@tngtech.com>
2019-05-13 09:13:11 +0200

    Switch to using a Map again

--- src/is-cjs.js
+++ src/is-cjs.js
@@ -1,14 +1,15 @@
-const isCjsPromises = Object.create(null);
+const isCjsPromises = new Map();
 
 export function getIsCjsPromise(id) {
-	let isCjsPromise = isCjsPromises[id];
+	let isCjsPromise = isCjsPromises.get(id);
 	if (isCjsPromise) return isCjsPromise.promise;
 
 	const promise = new Promise(resolve => {
-		isCjsPromises[id] = isCjsPromise = {
+		isCjsPromise = {
 			resolve,
 			promise: undefined
 		};
+		isCjsPromises.set(id, isCjsPromise);
 	});
 	isCjsPromise.promise = promise;
 
@@ -16,13 +17,13 @@ export function getIsCjsPromise(id) {
 }
 
 export function setIsCjsPromise(id, promise) {
-	const isCjsPromise = isCjsPromises[id];
+	const isCjsPromise = isCjsPromises.get(id);
 	if (isCjsPromise) {
 		if (isCjsPromise.resolve) {
 			isCjsPromise.resolve(promise);
 			isCjsPromise.resolve = undefined;
 		}
 	} else {
-		isCjsPromises[id] = { promise, resolve: undefined };
+		isCjsPromises.set(id, { promise, resolve: undefined });
 	}
 }
